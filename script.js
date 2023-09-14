d40aaa5e00c34ff2a1911bc8a2581c9b2676f75f
Siva Prasad K <122608989+kvdsp14@users.noreply.github.com>
2023-09-14 17:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	var char;
+  
+    for(var i = 0; i < str.length; i++){
+      if(str.indexOf(str[i]) === str.lastIndexOf(str[i])){
+        char = str[i];
+        break;
+      }
+    }
+  
+    return char;
+  }
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
